19645f51842d0e860b794d5c0a5cdc60053de6fd
Last Three <1563119813@qq.com>
2025-02-03 22:14:05 +0800

    Update Remove.sh

--- Bash/Remove.sh
+++ Bash/Remove.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo -e "\n即将在删除所有服务及文件夹"
+read -p "确认替换？(y/n) " confirm
+
+if [[ $confirm != [yY] ]]; then
+  echo "操作已取消"
+  exit 1
+fi
+
 docker stop $(docker ps -aq)
 
 docker rm $(docker ps -aq)
