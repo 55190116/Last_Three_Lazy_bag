7153b31b8d171b32042a1893627a13b9ebe8a610
Last Three <1563119813@qq.com>
2025-02-11 01:03:36 +0800

    Update Last_Three_Lazy_bag.sh

--- Last_Three_Lazy_bag.sh
+++ Last_Three_Lazy_bag.sh
@@ -19,9 +19,9 @@ if [ "$user_password" = "$correct_password" ]; then
     	"docker-compose-8.yml https://gh.llkk.cc/https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Lazy_bag/refs/heads/main/Compose/docker-compose-8.yml"
     	"docker-compose-9.yml https://gh.llkk.cc/https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Lazy_bag/refs/heads/main/Compose/docker-compose-9.yml"
     	"docker-compose-10.yml https://gh.llkk.cc/https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Lazy_bag/refs/heads/main/Compose/docker-compose-10.yml"
-    	"CreateDir.sh https://gh.llkk.cc/https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Lazy_bag/refs/heads/main/Bash/CreateDir.sh"
-    	"ModifyDir.sh https://gh.llkk.cc/https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Lazy_bag/refs/heads/main/Bash/ModifyDir.sh"
-        "Remove.sh https://gh.llkk.cc/https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Lazy_bag/refs/heads/main/Bash/Remove.sh"
+    	"CreateDir.sh https://gh.llkk.cc/https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Lazy_bag/refs/heads/main/Script/CreateDir.sh"
+    	"ModifyDir.sh https://gh.llkk.cc/https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Lazy_bag/refs/heads/main/Script/ModifyDir.sh"
+        "Remove.sh https://gh.llkk.cc/https://raw.githubusercontent.com/ATaKi-Myt/Last_Three_Lazy_bag/refs/heads/main/Script/Remove.sh"
     )
 
     for file_info in "${files[@]}"; do
@@ -97,19 +97,19 @@ if [ "$user_password" = "$correct_password" ]; then
     }
 
     echo "---------------------------------------- 博客地址：blog.010322.xyz -----------------------------------------------------------------"
-    echo "请选择要运行的服务："
+    echo "请选择要运行的服务（请先执行1和2）："
     echo "1.   更改根目录文件夹"
-    echo "2.   安装全部服务"
-    echo "3.   仅安装视频服务 nastool 、 jellyfin 、 qbittorrent 、 transmission 、 iyuuplus 、 xunlei 、jackett 、 alist-tvbox 、 aipan "
-    echo "4.   仅安装音乐服务 navidrome music-tag-web "
-    echo "5.   仅安装小说服务 reader "
-    echo "6.   仅安装漫画服务 komga "
-    echo "7.   仅安装办公服务 WPS 、 微力同步 、 PDF工具箱 、 在线预览文件 、 思源笔记 、 Alist "
-    echo "8.   仅安装网络服务 v2raya 、 frpc"
-    echo "9.   仅安装美化服务 sun-panel "
-    echo "10.  仅安装娱乐服务 GBA模拟器 "
-    echo "11.  仅安装系统服务 青龙面板"
-    echo "12.  创建其余文件夹 Video子目录 、 媒体源目录"
+    echo "2.   创建根目录文件夹" 
+    echo "3.   安装全部服务"
+    echo "4.   仅安装视频服务 nastool 、 jellyfin 、 qbittorrent 、 transmission 、 iyuuplus 、 xunlei 、jackett 、 alist-tvbox 、 aipan "
+    echo "5.   仅安装音乐服务 navidrome music-tag-web "
+    echo "6.   仅安装小说服务 reader "
+    echo "7.   仅安装漫画服务 komga "
+    echo "8.   仅安装办公服务 WPS 、 微力同步 、 PDF工具箱 、 在线预览文件 、 思源笔记 、 Alist "
+    echo "9.   仅安装网络服务 v2raya 、 frpc"
+    echo "10.  仅安装美化服务 sun-panel "
+    echo "11.  仅安装娱乐服务 GBA模拟器 "
+    echo "12.  仅安装系统服务 青龙面板"
     echo "13.  删除本地所有服务及文件夹"
     echo "0.   退出三人行懒人包安装脚本"
     echo "---------------------------------------- 博客地址：blog.010322.xyz -----------------------------------------------------------------"
@@ -125,57 +125,57 @@ if [ "$user_password" = "$correct_password" ]; then
                     1)
                         install_modify_dir_service
                         ;;
-                    2)
-                        install_all_service
+		    2)
+                        install_create_dir_service
                         ;;
                     3)
-                        install_video_service
+                        install_all_service
                         ;;
                     4)
-                        install_music_service
+                        install_video_service
                         ;;
                     5)
-                        install_novel_service
+                        install_music_service
                         ;;
                     6)
-                        install_comic_service
+                        install_novel_service
                         ;;
                     7)
-                        install_office_service
+                        install_comic_service
                         ;;
                     8)
-                        install_network_service
+                        install_office_service
                         ;;
                     9)
-                        install_beautify_service
+                        install_network_service
                         ;;
                     10)
+                        install_beautify_service
+                        ;;
+                    11)
                         install_game_service
                         ;;
-		    11)
+		    12)
                         install_system_service
-                        ;;
-                    12)
-                        install_create_dir_service
                         ;;
                     13)
                         install_remove_service
                         ;;
                 esac
 		echo "---------------------------------------- 博客地址：blog.010322.xyz -----------------------------------------------------------------"
-		echo "请选择要运行的服务："
+		echo "请选择要运行的服务（请先执行1和2）："
 		echo "1.   更改根目录文件夹"
-		echo "2.   安装全部服务"
-		echo "3.   仅安装视频服务 nastool 、 jellyfin 、 qbittorrent 、 transmission 、 iyuuplus 、 xunlei 、jackett 、 alist-tvbox 、 aipan "
-		echo "4.   仅安装音乐服务 navidrome music-tag-web "
-		echo "5.   仅安装小说服务 reader "
-		echo "6.   仅安装漫画服务 komga "
-		echo "7.   仅安装办公服务 WPS 、 微力同步 、 PDF工具箱 、 在线预览文件 、 思源笔记 、 Alist "
-		echo "8.   仅安装网络服务 v2raya 、 frpc"
-		echo "9.   仅安装美化服务 sun-panel "
-		echo "10.  仅安装娱乐服务 GBA模拟器 "
-  		echo "11.  仅安装系统服务 青龙面板 "
-		echo "12.  创建其余文件夹 Video子目录 、 媒体源目录"
+		echo "2.   创建根目录文件夹" 
+		echo "3.   安装全部服务"
+		echo "4.   仅安装视频服务 nastool 、 jellyfin 、 qbittorrent 、 transmission 、 iyuuplus 、 xunlei 、jackett 、 alist-tvbox 、 aipan "
+		echo "5.   仅安装音乐服务 navidrome music-tag-web "
+		echo "6.   仅安装小说服务 reader "
+		echo "7.   仅安装漫画服务 komga "
+		echo "8.   仅安装办公服务 WPS 、 微力同步 、 PDF工具箱 、 在线预览文件 、 思源笔记 、 Alist "
+		echo "9.   仅安装网络服务 v2raya 、 frpc"
+		echo "10.  仅安装美化服务 sun-panel "
+		echo "11.  仅安装娱乐服务 GBA模拟器 "
+		echo "12.  仅安装系统服务 青龙面板"
 		echo "13.  删除本地所有服务及文件夹"
 		echo "0.   退出三人行懒人包安装脚本"
 		echo "---------------------------------------- 博客地址：blog.010322.xyz -----------------------------------------------------------------"
