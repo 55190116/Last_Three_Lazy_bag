f695c8f415539313a8670a4aed18906c88666c78
Last Three <1563119813@qq.com>
2025-02-20 15:52:44 +0800

    Update ModifyDir.sh

--- Script/ModifyDir.sh
+++ Script/ModifyDir.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-read -p "请输入替换后的新路径（例如 /vol2/1000/）: " new_path
+echo "飞牛路径示例：/vol1/1000/"
+echo "群晖路径示例：/volume1/My/ My：根路径名称"
+echo "绿联旧系统路径示例：/mnt/dm-1/.ugreen_nas/509155/ 509155：用户文件名"
+read -p "请输入替换后的新路径: " new_path
 
 echo -e "\n即将在所有 compose.yml 文件中执行替换:"
 echo "/vol1/1000/ → [新路径] $new_path"
