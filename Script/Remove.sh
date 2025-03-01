dbb8e2c5c738a54a35faf38a22224ff62a6c996d
Last Three <1563119813@qq.com>
2025-03-01 20:50:09 +0800

    Update Remove.sh

--- Script/Remove.sh
+++ Script/Remove.sh
@@ -8,10 +8,10 @@ if [[ $confirm != [yY] ]]; then
   exit 1
 fi
 
-docker stop $(docker ps -aq)
+docker stop $(docker ps -aq) \
 
-docker rm $(docker ps -aq)
+docker rm $(docker ps -aq) \
 
-docker rmi $(docker images -aq)
+docker rmi $(docker images -aq) \
 
 rm -rf /vol1/1000/*/
