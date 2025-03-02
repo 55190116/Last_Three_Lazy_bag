12c70e45c0071b8af2fec60aee579e9ccfdbdadd
Last Three <1563119813@qq.com>
2025-03-02 13:38:33 +0800

    Add files via upload

---
+++ Intranet Penetration/baidu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /usr/local/bin/ \
+
+wget https://github.com/qjfoidnh/BaiduPCS-Go/releases/download/v3.9.7/BaiduPCS-Go-v3.9.7-linux-amd64.zip \
+
+unzip BaiduPCS-Go-v3.9.7-linux-amd64.zip \
+
+BaiduPCS-Go -v
